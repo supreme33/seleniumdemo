3ddf6fa1858ab1200c3a5015f5e6c7d94da8c743
Supreme <Supreme@DESKTOP-TNBFE61>
2018-04-10 15:59:03 +0530

    adding aditional testcases

---
+++ seleniumdemo/src/demo/demo5.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class demo5 {
+	@Test
+	public void  testcase1(){
+		WebDriver d1=new FirefoxDriver();
+		d1.get("http:\\www.google.com");
+		d1.get("http:\\www.facebook.com");
+		d1.findElement(By.id("email")).sendKeys("supreme@gmail.com");
+		d1.findElement(By.id("pass")).sendKeys("123456");
+		d1.quit();
+	}
+
+}
